7a9f0df0b908563f0f41908f0f702dca20b62de0
Jeong-minji <alswl4152@naver.com>
2022-01-13 12:53:31 +0900

    Add: Counter 예제 추가

---
+++ src/store/reducers/index.js
@@ -0,0 +1,8 @@
+// root reducer
+import { combineReducers } from "@reduxjs/toolkit";
+import countReducer from "./countReducer";
+
+export default combineReducers({ countReducer });
+
+// redux 에서 제공하는 combineReducers 를 통해 주제별로 나눈 조각 reducer들을 rootReducer 로 모아줍니다.
+// rootReducer는 전체 파일을 렌더하고 있는 index.js에서 사용 가능
